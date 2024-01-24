3b7eafd048edd1e445ba2a8dcb155b519ddd40df
camilotoco <91711193+camilotoco@users.noreply.github.com>
2024-01-24 08:44:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const h1 = document.querySelector('h1');
+aconst h1 = document.querySelector('h1');
 
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
@@ -9,6 +9,6 @@ btn.addEventListener('click', btnOnClick)
 
 function btnOnClick(){
 const suma = Number(input1.value) + Number(input2.value);
-resultado.innerText = "Resultado " + suma
+resultado.innerText = "Resultado de la suma " + suma
 
 }
