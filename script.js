c67d5964d7137eaadb40ac760e2da70f754dc1c7
camilotoco <camilotoco@gmail.com>
2024-01-24 07:55:14 -0500

    Agregar .gitignore y primeros arcivos del proyecto

---
+++ script.js
@@ -0,0 +1,14 @@
+const h1 = document.querySelector('h1');
+
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalculo');
+const resultado = document.querySelector('#result');
+
+btn.addEventListener('click', btnOnClick)
+
+function btnOnClick(){
+const suma = Number(input1.value) + Number(input2.value);
+resultado.innerText = "Resultado " + suma
+
+}
