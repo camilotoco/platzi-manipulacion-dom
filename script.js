78c3fdd975a6912daf8a3e5a6eeda3d62a13f1f1
camilotoco <camilotoco@gmail.com>
2024-01-25 09:59:12 -0500

    cambios

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-aconst h1 = document.querySelector('h1');
+const h1 = document.querySelector('h1');
 
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
